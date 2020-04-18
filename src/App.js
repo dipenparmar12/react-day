68e9a3f3c05178c0a4857a883866599c0694322d
dipen parmar <dipensavji@gmail.com>
2020-04-18 15:37:15 +0530

    Added Toggle, LoginController

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React from 'react';
 // import {} from "./Todolist/TodoList.css";
 // import {} from "./TicToc/TicToc.module.css";
-// import Game from "./TicToc/Game";
-// import HelloWorld from "./HelloWorld/HelloWorld";
-import ClickTic from "./Practice/ClickTic";
+// import Game from './TicToc/Game';
+// import HelloWorld from './HelloWorld/HelloWorld';
+// import ClickTic from './Practice/ClickTic';
+// import LoginControl from './Practice/LoginControl';
+// import Toggle from './Practice/Toggle';
 
 function App() {
   return (
     <div className="App">
-      <ClickTic />
+      {/* <HelloWorld /> */}
+      {/* <ClickTic /> */}
+      {/* <LoginControl /> */}
+      {/* <Toggle /> */}
     </div>
   );
 }
