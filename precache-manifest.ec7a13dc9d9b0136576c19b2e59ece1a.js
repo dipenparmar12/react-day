8a9ad95322cd48196941567673a4e9c6d0aa5910
dipen parmar <dipensavji@gmail.com>
2020-04-26 21:21:41 +0530

    Updates

---
+++ precache-manifest.ec7a13dc9d9b0136576c19b2e59ece1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb5912357d07995e6ea259a5a6286d3",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "24e9ad1cb6e08b277763",
+    "url": "/react/static/css/main.0b8b6e0a.chunk.css"
+  },
+  {
+    "revision": "a729972ce1e3b0ae9a5e",
+    "url": "/react/static/js/2.65ca5f09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.65ca5f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e9ad1cb6e08b277763",
+    "url": "/react/static/js/main.afba143f.chunk.js"
+  },
+  {
+    "revision": "c54ca87f16bbe4071d3f",
+    "url": "/react/static/js/runtime-main.0edea775.js"
+  }
+]);
