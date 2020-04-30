6d24469f995d3bb1398510cc87c466b68fea743a
dipen parmar <dipensavji@gmail.com>
2020-04-30 13:54:07 +0530

    Day10 Build done

---
+++ build/precache-manifest.77e1699fc594b9d90ef74f6e7f6a342e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f7bbc1f974f2c11659862163e08c4d",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "01cf299d2f1acd2c6163",
+    "url": "/react/static/css/main.ad9ef55b.chunk.css"
+  },
+  {
+    "revision": "f5fcec31f8f2bd5fc9a6",
+    "url": "/react/static/js/2.59917b0d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react/static/js/2.59917b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01cf299d2f1acd2c6163",
+    "url": "/react/static/js/main.14656c90.chunk.js"
+  },
+  {
+    "revision": "890a3fff554a1723b248",
+    "url": "/react/static/js/runtime-main.8e81cb53.js"
+  }
+]);
